32959cddfc000a8d563fa954c4aa7530f2a625e4
justinli34 <justinlibc@gmail.com>
2026-04-09 01:56:27 -0700

    add scroll to top on new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import "./index.css";
 import { Navigate, Route, Routes } from "react-router";
+import ScrollToTop from "./components/ScrollToTop";
 import BlogPostPage from "./pages/BlogPostPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="blog/:slug" element={<BlogPostPage />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="blog/:slug" element={<BlogPostPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   );
 }
 
